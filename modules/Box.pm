97bef74c6bd8a5cfc22b642c8ae28304ce2c9f63
Richard Forth <richard.forth@gmail.com>
2018-10-25 22:29:50 +0100

    Colors - I haz dem

--- modules/Box.pm
+++ modules/Box.pm
@@ -1,10 +1,12 @@
 package Box;
 
 sub debug {
+	my ($BOLD, $BLUE, $ENDC) = @_;
 	print "[ ${BOLD}${BLUE}??${ENDC} ] "; 
 }
 
 sub advisory {
+	my ($BOLD, $YELLOW, $ENDC) = @_;
 	print "[ ${BOLD}${YELLOW}\@\@${ENDC} ] "; 
 }
 
@@ -14,14 +16,17 @@ sub info {
 }
 
 sub ok {
+	my ($BOLD, $GREEN, $ENDC) = @_;
 	print "[ ${BOLD}${GREEN}OK${ENDC} ] ";
 }
 
 sub warn {
+	my ($BOLD, $YELLOW, $ENDC) = @_;
 	print "[ ${BOLD}${YELLOW}>>${ENDC} ] ";
 }
 
 sub crit {
+	my ($BOLD, $RED, $ENDC) = @_;
 	print "[ ${BOLD}${RED}!!${ENDC} ] ";
 }
 
